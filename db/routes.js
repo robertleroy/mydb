9f9369c4b09b5bb60b3e8a59f5d6fd474b285bf4
Rob Leroy <robleroy@msn.com>
2023-08-22 10:48:48 -0500

    gave up on corsm back to server side

--- db/routes.js
+++ db/routes.js
@@ -1,19 +1,13 @@
 const express = require("express");
-// const cors = require('cors');
 const router = express.Router();
 const controllers = require("./controllers");
 
 router.use(express.json());
-// router.use(cors({
-//   origin: '*'
-// }));
-
 
 router.get("/", controllers.getData);
 router.get("/:id", controllers.getItem);
 router.post("/", controllers.addItem);
 router.put("/:id", controllers.updateItem);
 router.delete("/:id", controllers.deleteItem);
-
 
 module.exports = router;
