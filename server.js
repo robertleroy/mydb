722963ee9a17b8ab62f2f84da6b19da39a75a147
Rob Leroy <robleroy@msn.com>
2023-08-22 10:55:45 -0500

    trying app.use(cors()) again

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require("express");
+const cors = require("cors");
+
 const app = express();
 const path = require("path");
 
 const dbRouter = require("./db/routes");
 
+app.use(cors());
 app.use('/db', dbRouter);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
